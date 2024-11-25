15429d6c56331e1e122d7e7f49ed25b8046f82ea
ThaisCarvalhoDosSantos3A2 <thais.mingau3@gmail.com>
2024-11-25 12:12:39 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,49 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'CSS',
+    'a) O que é CSS?',
+    'É uma linguagem para estilizar páginas HTML'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'CSS',
+    'b) Para que serve id no CSS?',
+    'Estlizar um elemento único'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'CSS',
+    'c) Como mudar a cor do fundo com CSS ?',
+    'Use background-color'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'HTML',
+    'd) Para que serve a tag < a >?',
+    'Criar links'
+)
+criaCartao(
+    'HTML',
+    'e) O que é uma tag < div >?',
+    'Um contêiner de bloco para agrupar elementos'
+)
+criaCartao(
+    'HTML',
+    'f) Como criar uma lista numerada?',
+    'Use a tag < ol > com < li > '
+)
+
+criaCartao(
+    'JavaScript',
+    'g) o que é JavaScript?',
+    'Linguagem para adicionar interatividade em págians web'
+)
+criaCartao(
+    'JavaScript',
+    'h) Como exibir uam mensagem de alerta?',
+    'Use alert () '
+)
+criaCartao(
+    'JavaScript',
+    'i) Para que serve o let no JavaScript?',
+    'Declarar variaveis come escopo de bloco'
 )
